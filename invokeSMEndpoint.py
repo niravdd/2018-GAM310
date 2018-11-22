d970d083f076b5930615b434dbeb2322e04a26dd
Nirav Doshi <niravdd@amazon.com>
2018-11-22 17:26:55 +1100

    Working demo... All associated resources & data...

---
+++ invokeSMEndpoint.py
@@ -0,0 +1,24 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    data = json.loads(json.dumps(event))
+    payload = data['data']
+    print(payload)
+
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME, ContentType='text/csv', Body=payload)
+    print(response)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+    pred = float(result)*100.00
+
+    return "Prediction?: " + str(pred) + "% fraudulent"
